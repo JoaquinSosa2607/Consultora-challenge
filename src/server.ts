aa6e6d95d53d15647caedef9248a696117005d77
Joaquin Sosa <129340097+JoaquinSosa2607@users.noreply.github.com>
2023-10-31 19:39:45 -0300

    using routes and starting server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
 import express from "express";
 import { AppDataSource } from "./data-source";
+import routes from "./routes/car.routes";
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 AppDataSource.initialize()
     .then(() => {
+        app.listen(3000);
         console.log("Connection ready");
+        console.log("Listening on 3000");
     })
     .catch((err) => {
         console.error(err);
-    }); 
+    });
